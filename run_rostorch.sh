4cffde24c5ece28c690331bb394b46a2c4020bf2
jral3s <53202448+jral3s@users.noreply.github.com>
2024-03-03 19:19:01 -0500

    Update run_rostorch.sh

--- run_rostorch.sh
+++ run_rostorch.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sudo docker run -it --rm -p 6081:6081 --privileged --runtime nvidia --device=/dev/ttyACM0 --group-add dialout --group-add input -v /dev:/dev -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/student_ws:/root/ros2_ws/src/student_ws --name racecar_hw -e DISPLAY=$DISPLAY d02454615ca8 # sebagarc/hardwareros2-v2
-
+sudo docker run -it --rm -p 6081:6081 --privileged --runtime nvidia --device=/dev/ttyACM0 --group-add dialout --group-add input -v ~/entrypoint.sh:/root/entrypoint.sh -v /dev:/dev -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/student_ws:/root/ros2_ws/src/student_ws --entrypoint /root/entrypoint.sh --name racecar_hw -e DISPLAY=$DISPLAY sebagarc/hardwareros2-production
