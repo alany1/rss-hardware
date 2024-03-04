7b8895e9b840dd75bcc266c5ed12c8be333336c4
Alan Yu <alanyu@csail.mit.edu>
2024-03-03 23:56:54 -0500

    Update run_rostorch.sh

--- run_rostorch.sh
+++ run_rostorch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run --entrypoint /root/racecar_ws/entrypoint.sh -it --rm -p 6081:6081 --privileged --runtime nvidia --device=/dev/ttyACM0 --group-add dialout --group-add input -v /dev:/dev -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/racecar_ws:/root//racecar_ws --name racecar_hw -e DISPLAY=$DISPLAY sebagarc/hardwareros2-production
+sudo docker run  -it --rm -p 6081:6081 --privileged --runtime nvidia --device=/dev/ttyACM0 --group-add dialout --group-add input -v ~/entrypoint.sh:/root/entrypoint.sh -v /dev:/dev -v /tmp/.X11-unix:/tmp/.X11-unix -v ~/racecar_ws:/root/racecar_ws --entrypoint /root/entrypoint.sh --name racecar_hw -e DISPLAY=$DISPLAY sebagarc/hardwareros2-production
