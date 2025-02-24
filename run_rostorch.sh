87b0f6b92b9c41f001282fb85b0822288f0526e0
Alan Yu <alanyu@csail.mit.edu>
2025-02-24 14:23:14 -0500

    update

--- run_rostorch.sh
+++ run_rostorch.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-sudo docker run -it --rm --runtime nvidia --gpus all -e DISPLAY=$DISPLAY -e NVIDIA_DRIVER_CAPABILITIES=all -v /tmp/.X11-unix:/tmp/.X11-unix --privileged -v /tmp/argus_socket:/tmp/argus_socket  -v /dev:/dev -v ~/racecar_ws:/root/racecar_ws --net=host  -e LD_LIBRARY_PATH=/usr/lib/aarch64-linux-gnu/tegra:$LD_LIBRARY_PATH -p 6901:6901 staffmitrss/racecar-real:latest --name racecar
+sudo docker run -it --rm --runtime nvidia --gpus all \
+  --name racecar \
+  -e DISPLAY=$DISPLAY \
+  -e NVIDIA_DRIVER_CAPABILITIES=all \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  --privileged \
+  -v /tmp/argus_socket:/tmp/argus_socket \
+  -v /dev:/dev \
+  -v ~/racecar_ws:/root/racecar_ws \
+  --net=host \
+  -e LD_LIBRARY_PATH=/usr/lib/aarch64-linux-gnu/tegra:$LD_LIBRARY_PATH \
+  -p 6901:6901 \
+  staffmitrss/racecar-real:latest
